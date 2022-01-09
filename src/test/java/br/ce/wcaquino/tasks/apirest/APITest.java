e9c095a4c2d538093ef0998f239a0a29d1e5422a
samuelssaquino <samuelssaquino@outlook.com>
2022-01-08 21:42:43 -0300

    first commit

---
+++ src/test/java/br/ce/wcaquino/tasks/apirest/APITest.java
@@ -0,0 +1,54 @@
+package br.ce.wcaquino.tasks.apirest;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class APITest {
+
+    @BeforeClass
+    public static void setup() {
+        RestAssured.baseURI = "http://localhost:8001/tasks-backend";
+    }
+
+    @Test
+    public void deveRetornarTarefa() {
+        RestAssured.given()
+            .when()
+                .get("/todo")
+            .then()
+                .log().all()
+                .statusCode(200)
+            ;
+    }
+
+    @Test
+    public void deveAdicionarTarefaComSucesso() {
+        RestAssured.given()
+                .body("{\"task\":\"Teste via API\",\"dueDate\":\"2022-01-30\"}")
+                .contentType(ContentType.JSON)
+            .when()
+                .post("/todo")
+            .then()
+                .log().all()
+                .statusCode(201)
+            ;
+    }
+
+    @Test
+    public void naoDeveRealizarTarefaInvalida() {
+        RestAssured.given()
+                .body("{\"task\":\"Teste via API\",\"dueDate\":\"2010-01-30\"}")
+                .contentType(ContentType.JSON)
+            .when()
+                .post("/todo")
+            .then()
+                .log().all()
+                .statusCode(400)
+                .body("message", CoreMatchers.is("Due date must not be in past"));
+            ;
+    }
+}
---
+++ src/test/java/br/ce/wcaquino/tasks/apirest/AppTest.java
@@ -0,0 +1,22 @@
+package br.ce.wcaquino.tasks.apirest;
+
+import static org.junit.Assert.assertEquals;
+
+// import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
